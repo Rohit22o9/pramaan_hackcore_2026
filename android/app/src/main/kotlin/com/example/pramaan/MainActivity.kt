fc8434a02f47d15d9017a774f195a0532a6bce4b
Rohit22o9 <rratnaparkhi3@gmail.com>
2026-08-10 11:40:11 +0530

    Intial Folder Structure

--- android/app/src/main/kotlin/com/example/pramaan/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pramaan
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ frontend/pramaan_app/android/app/src/main/kotlin/com/example/pramaan_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pramaan_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
